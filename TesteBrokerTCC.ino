5d63d95db0034bdc0e3eb165f33c05d50bc6ef0d
CarlosHZB <97548187+CarlosHZB@users.noreply.github.com>
2023-12-21 13:58:34 -0300

    Update TesteBrokerTCC.ino

--- TesteBrokerTCC.ino
+++ TesteBrokerTCC.ino
@@ -1,23 +1,36 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include "Stepper.h" 
+#include <Servo.h>
+#include <ESP8266HTTPClient.h>
+
+Servo myservo;
+
+int PassosPorVolta = 100; 
+Stepper MotorStepper(PassosPorVolta, 2, 4, 3, 5); 
 
 // Configuração da rede Wi-Fi
-const char* ssid = "carlos_net";
-const char* password = "braganet";
+const char* ssid = "Carlos HZB";
+const char* password = "12345678";
 
 // Configuração do broker MQTT
-const char* mqttServer = "ec2-52-67-169-181.sa-east-1.compute.amazonaws.com";
+const char* mqttServer = "broker.emqx.io";
 const int mqttPort = 1883; // Porta MQTT padrão
 const char* mqttTopic = "open"; // Tópico MQTT para escutar
 
 // Configuração do LED
-const int ledPin = D8; // Pino do LED no ESP8266
+const int ledPin = D2; // Pino do LED no ESP8266
+const int reedSwitchPin = D1; 
+int openCorrect = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  MotorStepper.setSpeed(300);
+  myservo.attach(D3);
+  pinMode(reedSwitchPin, INPUT_PULLUP);
   Serial.begin(9600);
   setup_wifi();
   client.setServer(mqttServer, 1883);
@@ -42,11 +55,41 @@ void setup_wifi() {
   Serial.println("Endereço IP: " + WiFi.localIP().toString());
 }
 
+void sendPostRequest() {
+  HTTPClient http;
+
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+  client->setInsecure();
+  
+  // Endpoint para enviar o POST request
+  const char* endpoint = "https://web-production-4b19.up.railway.app/alerts";
+
+  // Body da requisição
+  const char* requestBody = "{\"message\":\"A sala foi aberta sem a liberação da fechadura\",\"classroomId\":\"5696c38a-8b13-454b-ad7b-b3b40bd32730\"}";
+
+  http.begin(*client, endpoint); // Inicia a conexão HTTP
+  http.addHeader("Content-Type", "application/json"); // Define o tipo de conteúdo como JSON
+  
+  // Envia o POST request e obtém a resposta
+  int httpResponseCode = http.POST(requestBody);
+
+  if (httpResponseCode > 0) {
+    Serial.print("POST request enviado com sucesso. Código de resposta: ");
+    Serial.println(httpResponseCode);
+  } else {
+    Serial.print("Falha ao enviar o POST request. Código de erro: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end(); // Encerra a conexão HTTP
+}
+
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Conectando ao broker MQTT...");
 
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client2")) {
       Serial.println("Conectado");
       client.subscribe(mqttTopic);
     } else {
@@ -59,6 +102,7 @@ void reconnect() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  int pos;
   Serial.print("Mensagem recebida no tópico: ");
   Serial.println(topic);
 
@@ -71,8 +115,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // Comparar a mensagem com o ID esperado
   String expectedId = "7f7bf473-d8f2-4be0-af41-25648bc12b26"; // Substitua pelo ID esperado
   if (payloadStr == expectedId) {
-    Serial.println("Deu certo, porra");
+    openCorrect = 15;
+    Serial.println("Deu certo");
     digitalWrite(ledPin, HIGH); // Acender o LED
+      for (pos = 0; pos <= 90; pos += 1) { 
+      myservo.write(pos);              
+      delay(30);                       
+    }
+    //delay(2000);
+    //for (pos = 90; pos >= 0; pos -= 1) { 
+    //  myservo.write(pos);              
+    //  delay(30);                       
+    //}
     delay(1000); // Manter o LED aceso por 1 segundo
     digitalWrite(ledPin, LOW); // Desligar o LED
   }
@@ -83,4 +137,11 @@ void loop() {
     reconnect();
   }
   client.loop();
+
+
+  if (digitalRead(reedSwitchPin) == LOW && openCorrect == 0) {
+    sendPostRequest(); 
+    delay(1000);
+
+  }
 }
